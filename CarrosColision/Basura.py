e1d05c18b5c7bd84846ccd693ac0ea5a4699027d
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-01-22 00:39:53 -0600

    format

--- CarrosColision/Basura.py
+++ CarrosColision/Basura.py
@@ -1,23 +1,29 @@
+import numpy as np
 import pygame
-from pygame.locals import *
 
 # Cargamos las bibliotecas de OpenGL
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+from pygame.locals import *
 
-import random
-import math
-import numpy as np
 
 # Clase para el cubo Basura
 class Basura:
     def __init__(self, position, texture_file):
         # Vertices del cubo
-        self.points = np.array([[-1.0, -1.0,  1.0], [ 1.0, -1.0,  1.0],
-                                [ 1.0, -1.0, -1.0], [-1.0, -1.0, -1.0],
-                                [-1.0,  1.0,  1.0], [ 1.0,  1.0,  1.0],
-                                [ 1.0,  1.0, -1.0], [-1.0,  1.0, -1.0]])
+        self.points = np.array(
+            [
+                [-1.0, -1.0, 1.0],
+                [1.0, -1.0, 1.0],
+                [1.0, -1.0, -1.0],
+                [-1.0, -1.0, -1.0],
+                [-1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0],
+                [1.0, 1.0, -1.0],
+                [-1.0, 1.0, -1.0],
+            ]
+        )
         self.Position = position  # Posición fija en el plano
         self.texture = self.load_texture(texture_file)
 
@@ -26,10 +32,20 @@ class Basura:
         texture_surface = pygame.image.load(texture_file)
         texture_data = pygame.image.tostring(texture_surface, "RGB", True)
         width, height = texture_surface.get_size()
-        
+
         texture_id = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, texture_id)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data)
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RGB,
+            width,
+            height,
+            0,
+            GL_RGB,
+            GL_UNSIGNED_BYTE,
+            texture_data,
+        )
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
         return texture_id
@@ -43,42 +59,65 @@ class Basura:
 
         glBegin(GL_QUADS)
         # Cara frontal
-        glTexCoord2f(0, 0); glVertex3fv(self.points[0])
-        glTexCoord2f(1, 0); glVertex3fv(self.points[1])
-        glTexCoord2f(1, 1); glVertex3fv(self.points[5])
-        glTexCoord2f(0, 1); glVertex3fv(self.points[4])
-        
+        glTexCoord2f(0, 0)
+        glVertex3fv(self.points[0])
+        glTexCoord2f(1, 0)
+        glVertex3fv(self.points[1])
+        glTexCoord2f(1, 1)
+        glVertex3fv(self.points[5])
+        glTexCoord2f(0, 1)
+        glVertex3fv(self.points[4])
+
         # Cara trasera
-        glTexCoord2f(0, 0); glVertex3fv(self.points[3])
-        glTexCoord2f(1, 0); glVertex3fv(self.points[2])
-        glTexCoord2f(1, 1); glVertex3fv(self.points[6])
-        glTexCoord2f(0, 1); glVertex3fv(self.points[7])
-        
+        glTexCoord2f(0, 0)
+        glVertex3fv(self.points[3])
+        glTexCoord2f(1, 0)
+        glVertex3fv(self.points[2])
+        glTexCoord2f(1, 1)
+        glVertex3fv(self.points[6])
+        glTexCoord2f(0, 1)
+        glVertex3fv(self.points[7])
+
         # Cara izquierda
-        glTexCoord2f(0, 0); glVertex3fv(self.points[0])
-        glTexCoord2f(1, 0); glVertex3fv(self.points[4])
-        glTexCoord2f(1, 1); glVertex3fv(self.points[7])
-        glTexCoord2f(0, 1); glVertex3fv(self.points[3])
-        
+        glTexCoord2f(0, 0)
+        glVertex3fv(self.points[0])
+        glTexCoord2f(1, 0)
+        glVertex3fv(self.points[4])
+        glTexCoord2f(1, 1)
+        glVertex3fv(self.points[7])
+        glTexCoord2f(0, 1)
+        glVertex3fv(self.points[3])
+
         # Cara derecha
-        glTexCoord2f(0, 0); glVertex3fv(self.points[1])
-        glTexCoord2f(1, 0); glVertex3fv(self.points[5])
-        glTexCoord2f(1, 1); glVertex3fv(self.points[6])
-        glTexCoord2f(0, 1); glVertex3fv(self.points[2])
-        
+        glTexCoord2f(0, 0)
+        glVertex3fv(self.points[1])
+        glTexCoord2f(1, 0)
+        glVertex3fv(self.points[5])
+        glTexCoord2f(1, 1)
+        glVertex3fv(self.points[6])
+        glTexCoord2f(0, 1)
+        glVertex3fv(self.points[2])
+
         # Cara superior
-        glTexCoord2f(0, 0); glVertex3fv(self.points[4])
-        glTexCoord2f(1, 0); glVertex3fv(self.points[5])
-        glTexCoord2f(1, 1); glVertex3fv(self.points[6])
-        glTexCoord2f(0, 1); glVertex3fv(self.points[7])
-        
+        glTexCoord2f(0, 0)
+        glVertex3fv(self.points[4])
+        glTexCoord2f(1, 0)
+        glVertex3fv(self.points[5])
+        glTexCoord2f(1, 1)
+        glVertex3fv(self.points[6])
+        glTexCoord2f(0, 1)
+        glVertex3fv(self.points[7])
+
         # Cara inferior
-        glTexCoord2f(0, 0); glVertex3fv(self.points[0])
-        glTexCoord2f(1, 0); glVertex3fv(self.points[1])
-        glTexCoord2f(1, 1); glVertex3fv(self.points[2])
-        glTexCoord2f(0, 1); glVertex3fv(self.points[3])
+        glTexCoord2f(0, 0)
+        glVertex3fv(self.points[0])
+        glTexCoord2f(1, 0)
+        glVertex3fv(self.points[1])
+        glTexCoord2f(1, 1)
+        glVertex3fv(self.points[2])
+        glTexCoord2f(0, 1)
+        glVertex3fv(self.points[3])
         glEnd()
 
         glDisable(GL_TEXTURE_2D)
         glPopMatrix()
-
